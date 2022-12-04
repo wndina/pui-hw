c3a57f9b1b8b7ab2d3c31a39ff89652262b6d016
wndina <112358065+wndina@users.noreply.github.com>
2022-12-04 18:19:43 -0500

    FP

---
+++ FP/js/draggable.js
@@ -0,0 +1,4 @@
+jQuery_3_6_0( function(){
+  jQuery_3_6_0('.gallery__item').draggable({multiple: true});
+  });
+
---
+++ FP/js/rotator.js
@@ -0,0 +1,8 @@
+
+jQuery_1_9_1(document).ready(function(){
+    jQuery_1_9_1(".flipper .rotate").textrotator({
+    animation: "flipUp",
+    speed: 2000
+    });
+    
+    });
