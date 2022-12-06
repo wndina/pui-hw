b41215ec509a067d4b244d331246978a838abf26
wndina <112358065+wndina@users.noreply.github.com>
2022-12-05 20:17:22 -0500

    FP

--- FP/js/draggable.js
+++ FP/js/draggable.js
@@ -2,3 +2,15 @@ jQuery_3_6_0( function(){
   jQuery_3_6_0('.gallery__item').draggable({multiple: true});
   });
 
+
+//   if (jQuery_3_6_0(window).width() >699) { 
+    
+//     $(function(){
+//       jQuery_3_6_0('.gallery__item').draggable({multiple: true});
+//    } );
+
+// } else{
+//   $(function(){
+//     $('.gallery__item').draggable({multiple: false});
+//   } );
+// }
